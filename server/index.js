2538ffa8868ffd330214f2bb7132c4f7764899c1
lukemandavid4 <lukemandavid4@gmail.com>
2024-07-31 16:33:50 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
+const userRoute = require("./routes/userRoute.js");
 const connectDB = require("./config/config");
+const app = express();
 
 app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(`/api/user`, userRoute);
 
 connectDB();
 const port = process.env.PORT || 3000;
